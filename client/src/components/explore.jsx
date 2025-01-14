1d6df48ffe47cc846700ae52070d39979c7afa31
Charlie <160505188+oneandonlycharlie@users.noreply.github.com>
2025-01-14 22:28:22 +0100

    build: post feeds with components including like and comment buttons.

--- client/src/components/explore.jsx
+++ client/src/components/explore.jsx
@@ -1,15 +1,29 @@
-
+import "../styles/explore.css"
 function Explore(){
 
+    const explorePosts = [1,2,3,4,5,6,7,8,9,10]
 
     return (
         <>
          <h2>This is your explore feed</h2>
-        <p>Recommendation on the right goes here too</p>
+        <section className="explore">
+            {explorePosts.map((i)=> (
+                <Post key={i}/>
+            ))}
+        </section>
         </>
     )
 }
 
+function Post(){
 
+
+    return (
+        <div className="explore-post">
+            <img src="" alt="explore post" />
+        </div>
+    )
+
+}
 
 export default Explore
--- client/src/components/feed.jsx
+++ client/src/components/feed.jsx
@@ -2,20 +2,47 @@ import ForYou from "./recommendations"
 import "../styles/feed.css"
 function Feed(){
 
+    const numbers = [1,2,3,4,5]
 
     return (
         <>
          <section className="feed">
-            <div className="post">A post</div>
-            <div className="post">Another post</div>
-            <div className="post">post</div>
-            <div className="post">postssss</div>
+            {numbers.map((i) => (
+               <Post key={i} />
+            ))}
          </section>
          {/* <ForYou /> */}
         </>
     )
 }
 
+function Post(){
 
+
+    return (
+       <div className="post">
+            <div className="handle">
+                <img src="" alt="profile pic" />
+                <span>username</span>
+                <span>posted at ..</span>
+            </div>
+            <div className="post-image">
+                <img src="" alt="post image" />
+            </div>
+            <div className="buttons">
+                <button>Like</button>
+                <button>Comments</button>
+                {/* To do: add a pop up display for comments */}
+            </div>
+            <div className="info">
+                <p>number of likes</p>
+                <span>username</span>
+                <span>postdescribtion goes here dfsdf</span>
+                <p><input type="text" name="" id="" placeholder="Add a comment..."/><button>Post</button></p>
+            </div>
+       </div> 
+    )
+
+}
 
 export default Feed
