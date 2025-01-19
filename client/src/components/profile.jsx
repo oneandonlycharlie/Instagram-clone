dda6d7848baf9ee02eacaa7ac782e50cba9de9f6
Charlie <160505188+oneandonlycharlie@users.noreply.github.com>
2025-01-20 00:01:45 +0100

    Build: add data to personal profile

--- client/src/components/profile.jsx
+++ client/src/components/profile.jsx
@@ -1,14 +1,64 @@
+import { useOutletContext } from "react-router-dom"
+import "../styles/profile.css"
 function Profile(){
+
+    const [userData,setUserData]=useOutletContext()
 
 
     return (
-        <>
-         <h2>This is your profile</h2>
-        <p>all of your posts are here</p>
-        </>
+        <section className="profile">
+            <Handle 
+                user={userData.user}
+                followees={userData.followees}
+            />
+            <AllPosts posts={userData.user.posts}/>
+        </section>
     )
 }
 
+function Handle({user, followees}){
 
+    console.log(user)
+    return (
+        <div className="handle">
+            <img src={user.avatar} alt="headshot" />
+            <div>   
+                <span>{user.userName}</span>
+                <button>Edit profile</button>
+            </div>
+            <div>
+                <span>number of posts - {user.posts.length}</span>
+                <span>number of followers - N/A</span>
+                <span>number of following - {followees.length} </span>
+            </div>
+            <span>{user.bio}</span>
+        </div>
+    )
+}
+
+function AllPosts({posts}){
+
+    return (
+        <section className="my-posts">
+            {posts.map((post)=> (
+                <Post key={post.id}
+                      post={post}
+                
+                />
+            ))}
+        </section>
+    )
+}
+
+function Post({post}){
+
+
+    return (
+        <div className="my-post">
+            <img src={post.image} alt="my post" />
+        </div>
+    )
+
+}
 
 export default Profile
