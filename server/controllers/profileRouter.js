71a649be91f36171c3726ea9ab0dfc483f6afa1c
Charlie <160505188+oneandonlycharlie@users.noreply.github.com>
2025-03-23 23:56:56 +0100

    Add router to recieve put request for profile update

--- server/controllers/profileRouter.js
+++ server/controllers/profileRouter.js
@@ -1,11 +1,14 @@
 const { Router } = require("express")
 const profileRouter = new Router()
-const {user, followees, recommendedUsers} = require("../utils/userData")
+const {user, followees, recommendedUsers} = require("../utils/demoUserData")
+const db = require("../models/queries")
+const pool = require("../models/pool")
 
-
-profileRouter.post("/", (req,res)=>{
+profileRouter.put("/", async(req,res)=>{
     console.log("received edit request")
     console.log(req.body)
+    const {id, name, bio} = req.body
+    await db.updateProfile(id,name,bio)
     console.log("writing in data in DB and redirect to home page")
     res.redirect('/api')
 })
